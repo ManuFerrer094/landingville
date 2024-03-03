9e7c41e70091d90f1707602761025791c96c853a
Manuel <manuferrer094@gmail.com>
2024-03-03 10:37:53 +0100

    pagination

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -17,7 +17,7 @@ export class LandingComponent implements OnInit {
   linkedin: string | undefined;
   github: string | undefined;
   foto: string | undefined;
-  username: string | undefined; // Nueva propiedad para almacenar el nombre de usuario
+  username: string | undefined;
 
   constructor(private route: ActivatedRoute, private csvService: CsvService, private router: Router) { }
 
@@ -27,7 +27,6 @@ export class LandingComponent implements OnInit {
       if (userIndex) {
         this.csvService.getUserData(userIndex).subscribe(
           (userData: any) => {
-            // Asignar otras propiedades como lo estás haciendo actualmente
             this.nombre = userData.nombre;
             this.rol = userData.rol;
             this.descripcion = userData.descripcion;
@@ -36,8 +35,6 @@ export class LandingComponent implements OnInit {
             this.linkedin = userData.linkedin;
             this.github = userData.github;
             this.foto = userData.foto;
-
-            // Asignar el nombre de usuario si está disponible en los datos del usuario
             this.username = userData.username;
           },
           error => {
